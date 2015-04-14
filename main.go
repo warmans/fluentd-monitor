861bf8b55fcc5391f3ac0916aea6d72ac7a3f316
swarman <stefan.warman@switchconcepts.com>
2015-04-14 10:02:49 +0100

    Fixed bug with multiple hosts. gofmt some files. update static files

--- main.go
+++ main.go
@@ -1,91 +1,92 @@
 package main
 
 import (
-    "github.com/gorilla/websocket"
-    "log"
-    "net/http"
-    "flag"
-    "os"
-    "github.com/warmans/fluentd-api-client/monitoring"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
+	"github.com/warmans/fluentd-api-client/monitoring"
 )
 
-var upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024, CheckOrigin: func(r *http.Request) bool { return true; }}
+var upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024, CheckOrigin: func(r *http.Request) bool { return true }}
 
 //-----------------------------------------------
 // HTTP Handler
 //-----------------------------------------------
 
 type WebsocketHandler struct {
-    Hub *Hub
+	Hub *Hub
 }
 
 func (handler *WebsocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Print("socket upgrade failed: ", err)
-        return
-    }
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("socket upgrade failed: ", err)
+		return
+	}
 
-    //open new client connection
-    client := &Client{sendQueue: make(chan []byte, 256), conn: conn}
+	//open new client connection
+	client := &Client{sendQueue: make(chan []byte, 256), conn: conn}
 
-    //join the hub
-    client.JoinHub(handler.Hub)
+	//join the hub
+	client.JoinHub(handler.Hub)
 
-    //unregister on connection close
-    defer client.Close()
+	//unregister on connection close
+	defer client.Close()
 
-    //start relaying messages to client
-    go client.StartSending()
+	//start relaying messages to client
+	go client.StartSending()
 
-    //start listening for client messages
-    client.StartListening();
+	//start listening for client messages
+	client.StartListening()
 }
 
 func InDevMode() bool {
-    if os.Getenv("DEV") == "true" {
-        return true
-    }
-    return false
+	if os.Getenv("DEV") == "true" {
+		return true
+	}
+	return false
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    if InDevMode() {
-        log.Print("Running in DEV mode")
-    }
+	if InDevMode() {
+		log.Print("Running in DEV mode")
+	}
 
-    hub := NewHub()
-    go hub.Run()
+	hub := NewHub()
+	go hub.Run()
 
-    monitor := NewMonitor(hub, []*monitoring.Host{monitoring.NewHost("127.0.0.1:24220")})
-    go monitor.Run()
+	monitor := NewMonitor(hub, []*monitoring.Host{monitoring.NewHost("127.0.0.1:24220")})
+	go monitor.Run()
 
-    //static assets
-    var staticFileServer http.Handler
-    if (InDevMode()) {
-        //in dev mode serve raw files
-        staticFileServer = http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
-    } else {
-        //in production use embedded files
-        staticFileServer = http.FileServer(FS(false))
-    }
-    http.Handle("/static/", staticFileServer)
+	//static assets
+	var staticFileServer http.Handler
+	if InDevMode() {
+		//in dev mode serve raw files
+		staticFileServer = http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
+	} else {
+		//in production use embedded files
+		staticFileServer = http.FileServer(FS(false))
+	}
+	http.Handle("/static/", staticFileServer)
 
-    //websocket
-    http.Handle("/ws", &WebsocketHandler{Hub: hub})
+	//websocket
+	http.Handle("/ws", &WebsocketHandler{Hub: hub})
 
-    //redirect / to static dir
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path == "/" {
-            log.Print("redirect")
-            http.Redirect(w, r, "/static/", 301)
-        }
-    });
+	//redirect / to static dir
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			log.Print("redirect")
+			http.Redirect(w, r, "/static/", 301)
+		}
+	})
 
-    if err := http.ListenAndServe(*flag.String("addr", ":8080", "http service address"), nil); err != nil {
-        log.Fatal("HTTP Server failed: ", err)
-    }
+	if err := http.ListenAndServe(*flag.String("addr", ":8080", "http service address"), nil); err != nil {
+		log.Fatal("HTTP Server failed: ", err)
+	}
 }
--- monitor.go
+++ monitor.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    "sync"
-    "log"
-    "time"
-    "encoding/json"
-    "bytes"
-    "github.com/warmans/fluentd-api-client/monitoring"
+	"bytes"
+	"encoding/json"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/warmans/fluentd-api-client/monitoring"
 )
 
 //-----------------------------------------------
@@ -14,129 +15,129 @@ import (
 //-----------------------------------------------
 
 type State struct {
-    ID                       string
-    Host                     string
-    HostUp                   bool
-    HostError                string
-    Timestamp                int64
-    PluginID                 string
-    PluginCategory           string
-    PluginType               string
-    PluginConfig             map[string]interface{}
-    OutputPlugin             bool
-    BufferQueueLength        []int
-    BufferTotalQueuedSize    []int
-    RetryCount               []int
+	ID                    string
+	Host                  string
+	HostUp                bool
+	HostError             string
+	Timestamp             int64
+	PluginID              string
+	PluginCategory        string
+	PluginType            string
+	PluginConfig          map[string]interface{}
+	OutputPlugin          bool
+	BufferQueueLength     []int
+	BufferTotalQueuedSize []int
+	RetryCount            []int
 }
 
 type FMonitor struct {
-    Hub             *Hub
-    Hosts           []*monitoring.Host
-    States          []*State
-    StatesLock      sync.RWMutex
+	Hub        *Hub
+	Hosts      []*monitoring.Host
+	States     []*State
+	StatesLock sync.RWMutex
 }
 
 func (mon *FMonitor) Run() {
 
-    go func(){
-        updateTicker := time.NewTicker(time.Second * 5)
-        for range updateTicker.C {
+	go func() {
+		updateTicker := time.NewTicker(time.Second * 5)
+		for range updateTicker.C {
 
-            //update all hosts in parallel
-            for _, host := range mon.Hosts {
-                go func() {
-                    host.Update()
-                    mon.AddState(host);
-                }();
-            }
-        }
-    }()
+			//update all hosts in parallel
+			for _, host := range mon.Hosts {
+				go func(host *monitoring.Host) {
+					host.Update()
+					mon.AddState(host)
+				}(host)
+			}
+		}
+	}()
 
-    broadcastTicker := time.NewTicker(time.Second * 1)
-    for range broadcastTicker.C {
+	broadcastTicker := time.NewTicker(time.Second * 1)
+	for range broadcastTicker.C {
 
-        //Create a JSON payload
-        buffer := &bytes.Buffer{}
-        encoder := json.NewEncoder(buffer)
+		//Create a JSON payload
+		buffer := &bytes.Buffer{}
+		encoder := json.NewEncoder(buffer)
 
-        if err := encoder.Encode(mon.GetStateUpdate()); err != nil {
-            log.Print("Failed to encode host stats payload");
-            continue
-        }
+		if err := encoder.Encode(mon.GetStateUpdate()); err != nil {
+			log.Print("Failed to encode host stats payload")
+			continue
+		}
 
-        //Broadcast to all clients
-        mon.Hub.Broadcast(buffer.Bytes())
-    }
+		//Broadcast to all clients
+		mon.Hub.Broadcast(buffer.Bytes())
+	}
 }
 
 func (mon *FMonitor) GetStateUpdate() []*State {
-    mon.StatesLock.RLock()
-    defer mon.StatesLock.RUnlock()
+	mon.StatesLock.RLock()
+	defer mon.StatesLock.RUnlock()
 
-    return mon.States
+	return mon.States
 }
 
 func (mon *FMonitor) AddState(host *monitoring.Host) {
-    mon.StatesLock.Lock()
-    defer mon.StatesLock.Unlock()
+	mon.StatesLock.Lock()
+	defer mon.StatesLock.Unlock()
 
-    //flatten trees into rows
-    for _, plugin := range host.Plugins.Plugins {
+	//flatten trees into rows
+	for _, plugin := range host.Plugins.Plugins {
 
-        newState := &State{
-            ID: host.Address+"::"+plugin.PluginId,
-            Host: host.Address,
-            HostUp: host.Online,
-            HostError: host.LastError,
-            Timestamp: time.Now().Unix(),
-            PluginID: plugin.PluginId,
-            PluginCategory: plugin.PluginCategory,
-            PluginType: plugin.Type,
-            PluginConfig: plugin.Config,
-            OutputPlugin: plugin.OutputPlugin,
+		newState := &State{
+			ID:             host.Address + "::" + plugin.PluginId,
+			Host:           host.Address,
+			HostUp:         host.Online,
+			HostError:      host.LastError,
+			Timestamp:      time.Now().Unix(),
+			PluginID:       plugin.PluginId,
+			PluginCategory: plugin.PluginCategory,
+			PluginType:     plugin.Type,
+			PluginConfig:   plugin.Config,
+			OutputPlugin:   plugin.OutputPlugin,
 
-            BufferQueueLength: []int{plugin.BufferQueueLength},
-            BufferTotalQueuedSize: []int{plugin.BufferTotalQueuedSize},
-            RetryCount: []int{plugin.RetryCount}}
+			BufferQueueLength:     []int{plugin.BufferQueueLength},
+			BufferTotalQueuedSize: []int{plugin.BufferTotalQueuedSize},
+			RetryCount:            []int{plugin.RetryCount}}
 
-        //attempt existing row update
-        update := false
-        for key, state := range mon.States {
-            if newState.ID == state.ID {
+		//attempt existing row update
+		update := false
+		for key, state := range mon.States {
+			if newState.ID == state.ID {
 
-                update = true
-                oldState := state
+				update = true
+				oldState := state
 
-                //replace old state
-                mon.States[key] = newState
+				//replace old state
+				mon.States[key] = newState
 
-                //some values should hold a running history so...
-                if len(oldState.BufferQueueLength) >= 59 {
-                    oldState.BufferQueueLength = oldState.BufferQueueLength[1:]
-                }
-                mon.States[key].BufferQueueLength = append(oldState.BufferQueueLength, newState.BufferQueueLength[0])
+				//some values should hold a running history so...
+				if len(oldState.BufferQueueLength) >= 59 {
+					oldState.BufferQueueLength = oldState.BufferQueueLength[1:]
+				}
+				mon.States[key].BufferQueueLength = append(oldState.BufferQueueLength, newState.BufferQueueLength[0])
 
-                if len(oldState.BufferTotalQueuedSize) >= 59 {
-                    oldState.BufferTotalQueuedSize = oldState.BufferTotalQueuedSize[1:]
-                }
-                mon.States[key].BufferTotalQueuedSize = append(oldState.BufferTotalQueuedSize, newState.BufferTotalQueuedSize[0])
+				if len(oldState.BufferTotalQueuedSize) >= 59 {
+					oldState.BufferTotalQueuedSize = oldState.BufferTotalQueuedSize[1:]
+				}
+				mon.States[key].BufferTotalQueuedSize = append(oldState.BufferTotalQueuedSize, newState.BufferTotalQueuedSize[0])
 
-                if len(oldState.RetryCount) >= 59 {
-                    oldState.RetryCount = oldState.RetryCount[1:]
-                }
-                mon.States[key].RetryCount = append(oldState.RetryCount, newState.RetryCount[0])
-            }
-        }
-        //else append new
-        if update == false {
-            mon.States = append(mon.States, newState)
-        }
-    }
+				if len(oldState.RetryCount) >= 59 {
+					oldState.RetryCount = oldState.RetryCount[1:]
+				}
+				mon.States[key].RetryCount = append(oldState.RetryCount, newState.RetryCount[0])
+			}
+		}
+		//else append new
+		if update == false {
+			mon.States = append(mon.States, newState)
+		}
+	}
 }
 
 func NewMonitor(hub *Hub, hosts []*monitoring.Host) *FMonitor {
-    return &FMonitor{
-        Hub: hub,
-        Hosts: hosts,
-        States: make([]*State, 0)}
+	return &FMonitor{
+		Hub:    hub,
+		Hosts:  hosts,
+		States: make([]*State, 0)}
 }
